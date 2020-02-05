a1528ae411e924bc051fd09595bbfd2fd1edb91f
Andrew Lu <awclu@ucdavis.edu>
2020-02-05 00:17:18 -0800

    initial files

---
+++ app/public/index.js
@@ -0,0 +1,135 @@
+    $(document).ready(function(){
+    //Entrance page overlay
+        $("#main").hide();
+        $(".contactForm").hide();
+        $("#border").hide();
+        $("#border2").hide();
+    
+        
+        
+    
+        var sound = $(".foot").prepend("<i class='icon fas fa-volume-mute'><p id = 'sound'>Click to turn volume on</p></i>");
+    
+        $(".fa-volume-mute").on("click", function(){
+            $(".fa-volume-mute").replaceWith("<i class='icon fas fa-volume-up'><p id = 'sound'>Now Scroll!</p></i>");
+        })
+       
+        
+        var letter = $(".box");
+        var letter2 = $(".box2");
+    
+        letter.hide();
+        letter2.hide();
+    
+        var audio = new Audio("./assets/sounds/ding.wav");
+    
+          
+        if (mobile.matches){
+            //Mobile doesn't work unfortunately so we removed it
+            $("#main").show();     
+                
+        }
+            letter.trigger('click');
+            //Obtain Mouse Scroll Information up or down
+    
+            window.addEventListener("wheel", event => {
+            
+            const delta = Math.sign(event.deltaY);
+            console.log(delta);
+            letter.show();
+            letter2.show();
+            
+            unlock = $(".box").attr("style");
+            unlock2 = $(".box2").attr("style");
+            // console.log(unlock);
+            // console.log(unlock2);
+    
+            //Honestly I'm pretty big brained, but to scale I just get width yeehaw
+            var width = document.getElementById("lock").offsetWidth;
+            //console.log(width);
+            
+            
+            
+            //if scrolling up 
+            if (delta == -1 && lockFlag == false){
+             
+                letter.animate({ top: "-=100px" }, "normal");
+                letter2.animate({ top: "+=100px" }, "normal");
+                
+            }
+            //if scrolling down
+            else if (delta == 1 && lockFlag == false){
+                
+                letter.animate({ top: "+=100px" }, "normal");
+                letter2.animate({ top: "-=100px" }, "normal");
+            }
+            
+            if (delta === 1){
+                limit.push(delta);
+            }
+            if (delta === -1){
+                limit2.push(delta);
+            }
+           
+            if (limit.length - limit2.length === 5){
+                lockFlag = true;
+                console.log("unlocked by flag");
+                console.log(unlock);
+                console.log(unlock2);
+                if (unlock == "top: 200px;"&& unlock2 == "top: 320px;"){
+                    console.log("unlocked");
+                    
+                    entrance();
+                }
+                else{
+                    setTimeout(ding, 1550);
+                    setTimeout(entrance, 1900);
+                }
+                
+            }
+            
+function ding(){
+        audio.play();
+}
+
+function entrance(){
+    if (enable){
+    audio.play();
+     }
+    console.log("unlocked");
+    lockFlag = true;
+                
+    $("#border").show();
+    $("#border2").show();
+    //Scaled width
+              
+    $("#border").animate({width: width+"px"},100);
+    $("#border2").animate({width: width+"px"},100);
+                
+            
+        border.addEventListener("transitionend", function(){
+            setTimeout(function(){
+                letter.fadeOut("slow");
+                letter2.fadeOut("slow");
+                $("#border").fadeOut("slow");
+                $("#border2").fadeOut("slow");
+                $(".icon").fadeOut("slow")
+                    
+                },700);
+                           
+            setTimeout(function(){
+                $("#lock").hide();
+                letter.hide();
+                letter2.hide();
+                enable = false;
+                $("#main").fadeIn("slow");
+                $(".pPage").hide();
+                    ;
+                },1200);
+    
+               
+               
+        });
+       
+}
+});  
---
+++ app/routes/html-routes.js
@@ -0,0 +1,41 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  
+
+  //To Homepage
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  //To registration page
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+  
+  //To user interface to add movies
+  app.get("/portfolio", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/portfolio.html"));
+    console.log(req.body.first_name);
+    });
+ 
+  app.get("/contact", function(req,res){
+    res.sendFile(path.join(__dirname, "../public/contact.html"));
+    console.log(req.body.first_name);  
+  })
+
+  
+    
+
+
+};
+
