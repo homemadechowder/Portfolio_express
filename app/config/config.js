bc555c4164d006679d505c50cb648a6c83752c90
Andrew Lu <awclu@ucdavis.edu>
2020-02-18 21:57:24 -0800

    deploy

--- app/config/config.js
+++ app/config/config.js
@@ -1,9 +1,11 @@
-{
+require('dotenv').config();
+module.exports = {
+
   "development": {
-    "username": "root",
-    "password": "password",
-    "database": "portfolio_db",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql",
     "operatorsAliases": false
   },
@@ -16,11 +18,9 @@
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
     "dialect": "mysql",
-    "operatorsAliases": false
+    
+    "use_env_variable": "JAWSDB_URL"
   }
 }
+
